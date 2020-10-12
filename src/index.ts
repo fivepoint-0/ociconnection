1dfca96624653cb45b0ab25f03f9c349c7ed3fe7
Luke Parker <lparker@tpx.com>
2020-10-12 10:44:18 -0400

    moved dat util

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import net from 'net'
 import crypto from 'crypto'
 import { Parser } from 'xml2js'
 import { BroadsoftDocument, BroadsoftXMLHelper } from './xml'
-import BroadsoftDataUtility from './dataUtil'
 
-class OCIConnection {
+export class OCIConnection {
   public sessionId: string
   private host: any
   private port: any
@@ -99,7 +98,38 @@ class OCIConnection {
   }
 }
 
-export {
-  OCIConnection,
-  BroadsoftDataUtility
+export class BroadsoftDataUtility {
+  public static sentenceToCamelCase(_s: string) {
+    if (_s.length < 2) { return _s }
+    let s = _s[0].toLowerCase()
+    let chars = _s.split('')
+    chars.shift()
+    chars.forEach((char, index, arr) => {
+        if (char == ' ') {
+            arr[index+1] = arr[index+1].toUpperCase()
+        } else {
+            s += char
+        }
+    })
+    return s
+  }
+  
+  public static parseOciTable(ociTable: any): any { 
+    let table: any = [];
+  
+    let colHeadings = ociTable.colHeading.map((heading: string) => {
+        return BroadsoftDataUtility.sentenceToCamelCase(heading);
+    });
+  
+    ociTable.row.forEach((colRoot: { col: any[] }) => {
+        //TODO: these abstract objects should be simpleTypes or complexTypes to ensure data typing
+        let colData: any = {}
+        colRoot.col.forEach((col: any, index: string | number) => {
+            colData[colHeadings[index]] = col;
+        })
+        table.push(colData)
+    })
+    
+    return table;
+  }
 }
